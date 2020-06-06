da142b64b31196cc52088e3a44ad24f10b3703a4
inthahouse <alan.nguy@outlook.com>
2020-06-06 17:41:52 -0400

    refactored 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import ButtonLink from '../components/Button'
+import MainHeader from '../components/MainHeader'
+import SmallHeader from '../components/SmallHeader'
 
 export default () => (
   <Layout>
-    <h1>You in tha wrong place..</h1>
+    <MainHeader headerText={'You in tha wrong place..'} alignment='center' />
+    <SmallHeader header={'Click below to find the light!'} margin='10px 0' />
     <ButtonLink
       path=''
       primaryColor={'white'}
